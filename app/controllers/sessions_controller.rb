3ea3da1e4b87b34e70185bb7d793fe70640842b3
helman101 <helmanandresortegon@gmail.com>
2021-02-24 14:18:26 +0100

    add methods sign_in and create to Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+  def sign_in; end
+
+  def create
+    @user = User.find_by(session_params)
+    if @user
+      session[:current_user_id] = session_params
+      redirect_to @user, notice: 'Sing in successfully'
+    else
+      session[:current_user_id] = nil
+      render 'sign_in', notice: 'user doesn\'t exist'
+    end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:name)
+  end
 end
