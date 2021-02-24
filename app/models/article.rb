c1d8404be61d957c688f8cbfa81b4bc842f6725a
helman101 <helmanandresortegon@gmail.com>
2021-02-24 15:52:49 +0100

    create association between articles and categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   validates :author_id, :title, :content, presence: true
 
+  has_and_belongs_to_many :categories
   belongs_to :author, class_name: 'User'
   has_many :votes
   has_many :voting_users, through: :votes, source: :user
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+
+  has_and_belongs_to_many :articles
 end
