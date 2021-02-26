71290dbee5f58e0193670c414ef63ba2d3ca6e64
helman101 <helmanandresortegon@gmail.com>
2021-02-26 11:30:23 +0100

    change article validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,7 @@
 class Article < ApplicationRecord
   validates :author_id, :title, :content, presence: true
-  validates :title, presence: true, length: { in: 6..20 }
-  validates :content, presence: true, length: { in: 6..1000 }
-  validates :categories, presence: true
+  validates :title, presence: true, length: { in: 6..200 }
+  validates :content, presence: true, length: { in: 6..10000 }
 
   has_and_belongs_to_many :categories
   belongs_to :author, class_name: 'User'
