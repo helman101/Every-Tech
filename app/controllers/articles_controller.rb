b29f6402fcb825f24af24ffe7809027e97c935cf
helman101 <helmanandresortegon@gmail.com>
2021-02-26 12:56:22 +0100

    fix completely association between articles and categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,12 +7,16 @@ class ArticlesController < ApplicationController
 
   def create
     @article = current_user.authored_articles.build(article_params)
-    @article.categories.build(name: params[:article][:categories][:name])
-    if @article.save
+    category = @article.categories.build(name: params[:article][:categories][:name])
+    if @article.save && category.save
       redirect_to article_path(@article), notice: 'Article created succesfully'
     else
       redirect_to new_article_path, alert: 'This article can\'t be create'
     end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private 
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,16 +1,14 @@
 module ArticlesHelper
-  def create_categories(array, article)
-    array.each do |category|
-      case category
-      when 'Video Games'
-        article.categories.build(name: 'Video Games')
-      when 'Computers'
-        article.categories.build(name: 'Computers')
-      when 'Phones'
-        article.categories.build(name: 'Phones')
-      when 'Robotics'
-        article.categories.build(name: 'Robotics')
-      end
+  def create_categories(category, article)
+    case category
+    when 'Video Games'
+      article.categories.build(name: 'Video Games')
+    when 'Computers'
+      article.categories.build(name: 'Computers')
+    when 'Phones'
+      article.categories.build(name: 'Phones')
+    when 'Robotics'
+      article.categories.build(name: 'Robotics')
     end
   end
 end
