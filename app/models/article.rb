a62418a510234f9e7200620a95d48da76e257c94
helman101 <helmanandresortegon@gmail.com>
2021-03-01 13:10:05 +0100

    add more validations and one attached image to Articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,11 @@
 class Article < ApplicationRecord
-  validates :author_id, :title, :content, presence: true
+  validates :title, :content, :image, presence: true
+  validates :title, length: { in: 6..200 }
+  validates :content, length: { in: 6..5000 }
 
   has_and_belongs_to_many :categories
   belongs_to :author, class_name: 'User'
   has_many :votes
   has_many :voting_users, through: :votes, source: :user
+  has_one_attached :image, dependent: :destroy
 end
