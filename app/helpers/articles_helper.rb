93362b5f7ba65363afb9dcc40ba8490ed86b8749
helman101 <helmanandresortegon@gmail.com>
2021-03-01 14:52:31 +0100

    change radio buttons in Artcles New view for check boxes

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,7 +1,7 @@
 module ArticlesHelper
   def create_categories(categories, article)
     categories.each do |key, value|
-      article.categories.create(id: value.to_i, name: key.to_s)
+      article.categories.create(id: value.to_i, name: key.to_s) unless value.nil?
     end
   end
 end
