41213a3d951c9c4c062441e55a51dc78fffdbdd0
helman101 <helmanandresortegon@gmail.com>
2021-03-01 13:05:54 +0100

    add Vote resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :users, except: [:edit, :update]
-  resources :articles
+  resources :articles do
+    resources :votes, only: [:create, :destroy]
+  end
   resources :categories, only: [:index, :show, :destroy]
 
   get 'user/sign_in', to: 'sessions#sign_in'
   post 'user/sign_in', to: 'sessions#create'
   delete 'user/log_out', to: 'sessions#log_out'
 
-  root 'categories#show'
+  root 'categories#index'
 end
