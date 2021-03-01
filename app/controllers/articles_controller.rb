853ec366f492775db2002f97948fefbc03709530
helman101 <helmanandresortegon@gmail.com>
2021-03-01 13:11:55 +0100

    refactor Articles controller methods and add an article helper method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,30 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
+  before_action :require_login, only: [:create, :new]
+
   def new
     @article = Article.new
   end
 
   def create
-    @article = current_user.articles.build(article_params)
+    @article = current_user.authored_articles.build(article_params)
     if @article.save
-      redirect_to 
+      create_categories(params[:article][:categories], @article)
+      redirect_to article_path(@article), notice: 'Article created succesfully'
     else
-      render :new, notice: 'impossible create this article'
+      redirect_to new_article_path, alert: 'This article can\'t be create'
     end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private 
 
   def article_params
-    params.require(:articles).permit(:)
+    params.require(:article).permit(:title, :content, :image)
   end
+
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,7 @@
 module ArticlesHelper
+  def create_categories(categories, article)
+    categories.each do |key, value|
+      article.categories.create(id: value.to_i, name: key.to_s)
+    end
+  end
 end
