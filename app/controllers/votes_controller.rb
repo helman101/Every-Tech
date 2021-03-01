c11339b50735447468bee2e4b36bc54e3734d127
helman101 <helmanandresortegon@gmail.com>
2021-03-01 13:15:44 +0100

    add create and destroy methods to Votes controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,21 @@
 class VotesController < ApplicationController
+  def create
+    @vote = current_user.votes.new(article_id: params[:article_id])
+
+    if @vote.save
+      redirect_to article_path(params[:article_id]), notice: 'You vote this post.'
+    else
+      redirect_to article_path(params[:article_id]), alert: 'You cannot vote this post.'
+    end
+  end
+
+  def destroy
+    vote = Vote.find_by(id: params[:id], user: current_user, article_id: params[:article_id])
+    if vote
+      vote.destroy
+      redirect_to article_path(params[:article_id]), notice: 'You disliked a post.'
+    else
+      redirect_to article_path(params[:article_id]), alert: 'You cannot dislike post that you did not like before.'
+    end
+  end
 end
