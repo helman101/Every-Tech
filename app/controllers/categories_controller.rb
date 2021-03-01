f03b0ee666af1d5ddd8fdca4489a3db198aef458
helman101 <helmanandresortegon@gmail.com>
2021-03-01 13:13:16 +0100

    add index and show controllers to Categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,11 @@
 class CategoriesController < ApplicationController
+  def index
+    @most_pop = Article.includes(:votes).group(:article_id).first
+    @categories = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
 end
