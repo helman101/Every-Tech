502b03d0dddc5cd0a54b82fa353c54d934a0c3db
helman101 <helmanandresortegon@gmail.com>
2021-03-02 09:21:07 +0100

    add a default value to Categories priority

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_120256) do
+ActiveRecord::Schema.define(version: 2021_03_02_081919) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2021_03_01_120256) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.integer "priority"
+    t.integer "priority", default: 1
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
